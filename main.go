3e9f0583dede068bfc4ec84192c3e7b3d4d3a780
Hemanth5544 <rachapalli.hemanth5544@gmail.com>
2025-05-27 18:29:09 +0530

    server details

--- main.go
+++ main.go
@@ -1,10 +1,39 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
-func main() {
+var processing = false
 
-	fmt.Println("Server running on :8080")
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	mmdbConnect()
+
+	// defer dbClose()
+	// initialise()
+
+	http.HandleFunc("GET /", getHome)
+	http.HandleFunc("GET /ip/{ip}", getIp)
+	http.HandleFunc("GET /random/{ipVersion}", getRandomIp)
+	http.HandleFunc("GET /benchmark/{ipVersion}/{times}", getBenchmark)
+
+	fmt.Printf("starting server on %s:%s\n", os.Getenv("SERVER_HOST"), os.Getenv("SERVER_PORT"))
+	err = http.ListenAndServe(fmt.Sprintf("%s:%s", os.Getenv("SERVER_HOST"), os.Getenv("SERVER_PORT")), nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Println("server closed")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }

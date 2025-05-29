84c6d76861756e8a8e1de3692ec18fe6c178ae99
Hemanth5544 <rachapalli.hemanth5544@gmail.com>
2025-05-29 18:39:35 +0530

    get ip route

--- routes.go
+++ routes.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 func getHome(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-Type", "application/json")
 	response.Write([]byte(`{ "message": "IP-Trace" }`))
 }
+
+func getIp(response http.ResponseWriter, request *http.Request) {
+
+
+	ipString := request.PathValue("ip")
+	fmt.Println("PathValue as params: " + ipString)
+
+
+	jsonBytes, err := fetchIPJson(ipString)
+	if err != nil {
+		response.Header().Set("Content-Type", "application/json")
+		response.Write([]byte(`{ "error": "` + err.Error() + `" }`))
+		return
+	}
+
+	response.Header().Set("Content-Type", "application/json")
+	response.Write(jsonBytes)
+}
+
+

51ca420b6f607814358b33dd3b040eba4c7bb8d1
Hemanth5544 <rachapalli.hemanth5544@gmail.com>
2025-06-06 14:36:58 +0530

    random ip

--- routes.go
+++ routes.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-	"time"
+
+	random "github.com/hemanth5544/iptrace/utils"
 )
 
 func getHome(response http.ResponseWriter, request *http.Request) {
@@ -14,10 +14,8 @@ func getHome(response http.ResponseWriter, request *http.Request) {
 
 func getIp(response http.ResponseWriter, request *http.Request) {
 
-
 	ipString := request.PathValue("ip")
 	fmt.Println("PathValue as params: " + ipString)
-
 
 	jsonBytes, err := fetchIPJson(ipString)
 	if err != nil {
@@ -30,4 +28,23 @@ func getIp(response http.ResponseWriter, request *http.Request) {
 	response.Write(jsonBytes)
 }
 
+func getRandomIp(response http.ResponseWriter, request *http.Request) {
 
+	var ipString string
+	ipVersion := request.PathValue("ipVersion")
+	if ipVersion == "4" {
+		ipString = random.RandomIpv4()
+	} else {
+		ipString = random.RandomIpv6()
+	}
+
+	jsonBytes, err := fetchIPJson(ipString)
+	if err != nil {
+		response.Header().Set("Content-Type", "application/json")
+		response.Write([]byte(`{ "error": "` + err.Error() + `" }`))
+		return
+	}
+
+	response.Header().Set("Content-Type", "application/json")
+	response.Write(jsonBytes)
+}

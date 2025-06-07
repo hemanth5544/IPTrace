d8b703c31c2d7b4e47e373ead014a8ac977ec98c
Hemanth5544 <rachapalli.hemanth5544@gmail.com>
2025-06-07 20:11:53 +0530

    ip ranges

--- helpers.go
+++ helpers.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"net"
 	"os"
 	"strings"
+
+	"github.com/seancfoley/ipaddress-go/ipaddr"
 )
 
 func hasASNDatabase() bool {
@@ -17,26 +20,34 @@ func hasCountryDatabase() bool {
 	return len(os.Getenv("COUNTRY")) > 0
 }
 
-func isIpv4Reserved(ip string) bool {
-	return strings.HasPrefix(ip, "0.") || strings.HasPrefix(ip, "127.") || strings.HasPrefix(ip, "10.") ||
-		strings.HasPrefix(ip, "100.64.") || strings.HasPrefix(ip, "169.254.") || strings.HasPrefix(ip, "172.16.") ||
-		strings.HasPrefix(ip, "172.17.") || strings.HasPrefix(ip, "172.18.") || strings.HasPrefix(ip, "172.19.") ||
-		strings.HasPrefix(ip, "172.20.") || strings.HasPrefix(ip, "172.21.") || strings.HasPrefix(ip, "172.22.") ||
-		strings.HasPrefix(ip, "172.23.") || strings.HasPrefix(ip, "172.24.") || strings.HasPrefix(ip, "172.25.") ||
-		strings.HasPrefix(ip, "172.26.") || strings.HasPrefix(ip, "172.27.") || strings.HasPrefix(ip, "172.28.") ||
-		strings.HasPrefix(ip, "172.29.") || strings.HasPrefix(ip, "172.30.") || strings.HasPrefix(ip, "172.31.") ||
-		strings.HasPrefix(ip, "192.0.0.") || strings.HasPrefix(ip, "192.0.2.") || strings.HasPrefix(ip, "192.88.") ||
-		strings.HasPrefix(ip, "192.168.") || strings.HasPrefix(ip, "198.18.") || strings.HasPrefix(ip, "198.19.") ||
-		strings.HasPrefix(ip, "198.51.100") || strings.HasPrefix(ip, "203.0.113") || strings.HasPrefix(ip, "224.") ||
-		strings.HasPrefix(ip, "225.") || strings.HasPrefix(ip, "226.") || strings.HasPrefix(ip, "227.") ||
-		strings.HasPrefix(ip, "228.") || strings.HasPrefix(ip, "229.") || strings.HasPrefix(ip, "230.") ||
-		strings.HasPrefix(ip, "231.") || strings.HasPrefix(ip, "232.") || strings.HasPrefix(ip, "233.") ||
-		strings.HasPrefix(ip, "234.") || strings.HasPrefix(ip, "235.") || strings.HasPrefix(ip, "236.") ||
-		strings.HasPrefix(ip, "237.") || strings.HasPrefix(ip, "238.") || strings.HasPrefix(ip, "239.") ||
-		strings.HasPrefix(ip, "240.") || strings.HasPrefix(ip, "241.") || strings.HasPrefix(ip, "242.") ||
-		strings.HasPrefix(ip, "243.") || strings.HasPrefix(ip, "244.") || strings.HasPrefix(ip, "245.") ||
-		strings.HasPrefix(ip, "246.") || strings.HasPrefix(ip, "247.") || strings.HasPrefix(ip, "248.") ||
-		strings.HasPrefix(ip, "249.") || strings.HasPrefix(ip, "250.") || strings.HasPrefix(ip, "251.") ||
-		strings.HasPrefix(ip, "252.") || strings.HasPrefix(ip, "253.") || strings.HasPrefix(ip, "254.") ||
-		strings.HasPrefix(ip, "255.")
+func getIpVersion(ipString string) int {
+	ipVersion := 4
+	if strings.Contains(ipString, ":") {
+		ipVersion = 6
+	}
+
+	return ipVersion
+}
+
+func findIPRanges(ipRangeStart string, ipRangeEnd string) []*net.IPNet {
+	ipStart := ipaddr.NewIPAddressString(ipRangeStart)
+	ipEnd := ipaddr.NewIPAddressString(ipRangeEnd)
+
+	addressStart := ipStart.GetAddress()
+	addressEnd := ipEnd.GetAddress()
+
+	ipRange := addressStart.SpanWithRange(addressEnd)
+	rangeSlice := ipRange.SpanWithPrefixBlocks()
+
+	var ipNets []*net.IPNet
+	for _, val := range rangeSlice {
+		_, network, err := net.ParseCIDR(val.String())
+		if err != nil {
+			panic(err)
+		}
+
+		ipNets = append(ipNets, network)
+	}
+
+	return ipNets
 }
